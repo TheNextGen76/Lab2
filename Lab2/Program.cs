6cce5ac295a29192aaecef1964703f9da0225c0e
The gungus <mikaelsson.viggo@gmail.com>
2024-11-29 11:58:06 +0100

    Scary Math
    läskig matte

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -11,16 +11,21 @@
             bool menu = true;
             while (menu)
             {
-                Console.WriteLine("\tWind Chill Calculator\n\n\t[1] Stuff #1\n\t[2] Stuff #2\n\t[3] Exit Program");
+                Console.WriteLine("\tWind Chill Calculator\n\n\t[1] Beräkna Windchill faktorn i Km/h #1\n\t[2] Beräkna windchill faktron i m/s #2\n\t[3] Exit Program");
                 Int32.TryParse(Console.ReadLine(), out int menuChoice);
                 switch (menuChoice)
                 {
                     case 1:
                         {
+                            Console.Clear();
+                            Console.WriteLine("Case 1");
+
                             break;
                         }
                     case 2:
                         {
+                            Console.Clear();
+                            Console.WriteLine("Case 2");
                             break;
                         }
                     case 3:
@@ -40,6 +45,12 @@
             }
             static void WCTCaalculation()
             {
+                //Ta in km/h från användaren ska kunna omvandla till m/s om nödväntigt
+                //utifrån det ska vi beräkna windchill och utifrån det så ska vi
+                //ska vi kunna placera det på windchill indexet
+                double T;
+                double V;
+                double wtc = 13.12 + 0.6215 * T - 11.37 * V ^ 0.16 + 0.3965 * T * V ^ 0.16;
 
             }
         }
