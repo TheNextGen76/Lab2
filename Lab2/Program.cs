230f3b4894c531d0ea8ff000c26f03390bc03bb1
Reckysfriend <36387302+Reckysfriend@users.noreply.github.com>
2024-11-29 11:33:15 +0100

    Added basic framework
    Added a menu and switch case along with a dummy function.

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double temprature;
+            double windSpeeds;
+            double wct;
+
+            bool menu = true;
+            while (menu)
+            {
+                Console.WriteLine("\tWind Chill Calculator\n\n\t[1] Stuff #1\n\t[2] Stuff #2\n\t[3] Exit Program");
+                Int32.TryParse(Console.ReadLine(), out int menuChoice);
+                switch (menuChoice)
+                {
+                    case 1:
+                        {
+                            break;
+                        }
+                    case 2:
+                        {
+                            break;
+                        }
+                    case 3:
+                        {
+                            menu = false;
+                            Environment.Exit(0);
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+
+                }
+
+
+            }
+            static void WCTCaalculation()
+            {
+
+            }
         }
     }
 }
+
