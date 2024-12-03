7e7f228a45c6a03edf7f51249d3d9503fc2f4371
Reckysfriend <36387302+Reckysfriend@users.noreply.github.com>
2024-12-03 21:43:52 +0100

    Added Index logic
    Added Index Logic and started handling some of the string outputs to be formatted correctly

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -4,44 +4,76 @@
     {
         static void Main(string[] args)
         {
-            double temprature;
-            double windSpeeds;
+            double temprature = 0;
+            string tempratureStr = "";
+            double windSpeeds = 0;
+            string windSpeedsStr = "";
             double wct;
+            string wctIndexStr = "";
             bool hasWindSpeed = false;
             bool hasTemprature = false;
 
             bool menu = true;
             while (menu)
             {
-                Console.WriteLine("\tWind Chill Calculator - Please enter the wind speed and temprature:" +
-                    "\n\n\t[1] Wind Speed\n\t[2] Temprature\n\t[3] Exit Program");
+                if (hasWindSpeed == true && hasTemprature == true) 
+                {
+                    wct = WCTCaalculation(temprature,windSpeeds);
+                    if (wct > -25) 
+                    {
+                        wctIndexStr = $"\tThe Wind Chill is {wct}°C which is - Cold";
+                    }
+                    else if (wct <= -25 && wct > -35)
+                    {
+                        wctIndexStr = $"\tThe Wind Chill is {wct}°C which is - Very Cold";
+                    }
+                    else if (wct <= -35 && wct > -60)
+                    {
+                        wctIndexStr = $"\tThe Wind Chill is {wct}°C which is - Danger of Frostbite";
+                    }
+                    else if (wct < -60)
+                    {
+                        wctIndexStr = $"\tThe Wind Chill is {wct}°C which is - Great Danger of Frostbite";
+                    }
+                    Console.WriteLine(wctIndexStr);
+                }
+                else
+                {
+                    Console.WriteLine($"\tWind Chill Calculator - Please enter the wind speed and temprature:");
+                }
+                    Console.WriteLine($"\n\t[1] Wind Speed {windSpeedsStr}\n\t[2] Temprature {tempratureStr}\n\n\t[3] Exit Program");
                 Int32.TryParse(Console.ReadLine(), out int menuChoice);
                 switch (menuChoice)
                 {
                     case 1:
                         {
                             Console.Clear();
-                            Console.WriteLine("");
+                            Console.WriteLine("Speed");
+                            Double.TryParse(Console.ReadLine(), out windSpeeds);
+                            windSpeedsStr = " - " + windSpeeds.ToString();
+                            hasWindSpeed = true;
 
                             break;
                         }
                     case 2:
                         {
+                            Console.Clear();
                             bool subMenu = true;
                             while (subMenu)
                             {
-                                Console.Clear();
-                                Console.Write("Enter a temprature in Celcius:");
+                                Console.Write("\tEnter a temprature in Celcius:");
                                 Double.TryParse(Console.ReadLine(), out temprature);
-                                if (temprature is Double)
+                                if (temprature != 0)
                                 {
-                                    hasTemprature |= true;
+                                    Console.Clear();
+                                    tempratureStr = " - " + temprature.ToString() + "°C ";
+                                    hasTemprature = true;
                                     subMenu = false;
                                 }
                                 else
                                 {
-                                    Console.WriteLine("This is not a temprature!");
-                                    Console.ReadLine();
+                                    Console.Clear();
+                                    Console.WriteLine("\tThis is not a temprature! Please enter a valid temprature.\n");
                                 }
                             }
                             break;
@@ -62,21 +94,17 @@
 
 
             }
-            static void WCTCaalculation(double temprature, double windSpeed)
+            static double WCTCaalculation(double temprature, double windSpeed)
             {
                 //Ta in km/h från användaren ska kunna omvandla till m/s om nödväntigt
                 //utifrån det ska vi beräkna windchill och utifrån det så ska vi
                 //ska vi kunna placera det på windchill indexet
-                double T = 2;
-                double V = 3.3;
                 double P = Math.Pow(windSpeed, 0.16);
                 double wtc = 13.12 + 0.6215 * temprature - 11.37 * P + 0.3965 * temprature * P;
-                Console.WriteLine(wtc);
+                wtc = Math.Round(wtc,2);
+                return wtc;
 
             }
-        }
-        static void WindSpeed()
-        {      
         }
     }
 }
